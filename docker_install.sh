cb324948e531b461f73cbb52ac8f28678bcfd7ac
tjmoon <tjmoon@vntgcorp.com>
2021-07-06 16:50:33 +0900

    update docker_install script

--- docker_install.sh
+++ docker_install.sh
@@ -4,16 +4,21 @@ sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
+    gnupg \
+    lsb-release
 
-sudo apt-get install -y docker-ce
-sudo systemctl enable docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
 grep -q docker /etc/group || sudo groupadd docker
-#sudo usermod -aG docker ubuntu
 sudo usermod -aG docker $USER
+newgrp docker
+#docker run hello-world
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
