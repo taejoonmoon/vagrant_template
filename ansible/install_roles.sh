906cd59fbced45e3cca3ccee7714022e3ef0033c
tjmoon <tjmoon@vntgcorp.com>
2021-07-09 19:30:45 +0900

    update install_roles script

--- ansible/install_roles.sh
+++ ansible/install_roles.sh
@@ -2,3 +2,4 @@
 test -d roles || mkdir roles
 #git clone git@github.com:VntgCorp/ansible-osconfig.git roles/osconfig
 ansible-galaxy install --roles-path roles/ -r requirements.yml
+#ansible-playbook -i localhost playbook.yml
