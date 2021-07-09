3eb5ba16105f9ba5b4635d0b676455fdb102c303
tjmoon <tjmoon@vntgcorp.com>
2021-07-09 19:18:11 +0900

    adding ansible-galaxy update scrtip

---
+++ ansible/install_roles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+test -d roles || mkdir roles
+#git clone git@github.com:VntgCorp/ansible-osconfig.git roles/osconfig
+ansible-galaxy install --roles-path roles/ -r requirements.yml
--- ansible/roles_install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git clone git@github.com:VntgCorp/ansible-osconfig.git roles/osconfig
