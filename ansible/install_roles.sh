ccb20a5b6433ce33916e67a3c0b7495719b8cea6
tjmoon <tjmoon@example.com>
2021-07-15 11:31:11 +0900

    update

--- ansible/install_roles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-test -d roles || mkdir roles
-#git clone git@github.com:VntgCorp/ansible-osconfig.git roles/osconfig
-ansible-galaxy install --roles-path roles/ -r requirements.yml
-#ansible-playbook -i localhost playbook.yml
